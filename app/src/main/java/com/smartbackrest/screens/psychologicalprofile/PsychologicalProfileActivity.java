c2d75a19de36bd1abb05c82f74cb42752068d9ba
jogendra <goudajogendra@gmail.com>
2021-05-14 19:06:33 +0530

    -- activity_psychological_profile

--- app/src/main/java/com/smartbackrest/screens/psychologicalprofile/PsychologicalProfileActivity.java
+++ app/src/main/java/com/smartbackrest/screens/psychologicalprofile/PsychologicalProfileActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.smartbackrest.ApplicationData;
 import com.smartbackrest.R;
@@ -18,6 +19,9 @@ public class PsychologicalProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_psychological_profile);
+        TextView title = findViewById(R.id.screen_title);
+        title.setText("Psychological Profile");
+
 
         findViewById(R.id.layoutHelp).setOnClickListener(new View.OnClickListener() {
             @Override
